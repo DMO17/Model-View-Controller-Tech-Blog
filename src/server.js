e8dcfa6e72f7bbe805f4bc9d02fa783f34162f47
Dahir Mohamed <dmoabdi98@gmail.com>
2021-12-27 23:22:17 +0000

    set up server with hbs and squelize

--- src/server.js
+++ src/server.js
@@ -1,8 +1,40 @@
+// npm packages imports
 const express = require("express");
+const expressHandleBars = require("express-handlebars");
+const path = require("path");
+
+// global imports
+const connection = require("./config/connection");
 
 const PORT = 5001;
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`navigate to http://localhost:${PORT}`);
-});
+//static middleware
+app.use(express.static(path.join(__dirname, "../public")));
+
+// set up handlebars
+const hbs = expressHandleBars.create({});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// body parser middleware
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(routes);
+
+const init = async () => {
+  try {
+    await connection.sync({ force: false });
+
+    console.log(`[INFO]: Connected to the DATABASE Successfully!!!`);
+
+    app.listen(PORT, () => {
+      console.log(`navigate to http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to connect to DB => ${error.message}`);
+  }
+};
+
+init();
